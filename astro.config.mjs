bd6ff263d441c843bd5676c54b0b5051681eb7ae
ymc0182 <501414437@qq.com>
2024-06-22 15:49:23 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [tailwind()],
-
 
   //主题配置（不用的注释掉）
   theme: {
