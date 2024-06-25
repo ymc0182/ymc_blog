9eddbb88635fb9c30bfa4c67ee3cb391fa4fd1e4
ymc0182 <501414437@qq.com>
2024-06-26 01:41:59 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,19 +10,4 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-
-  //主题配置（不用的注释掉）
-  theme: {
-    //网站名称
-    siteTitle: "",
-
-    //网站描述
-    siteDescription: "",
-
-    //网站关键字
-    siteKeywords: ['Astro', 'Blog'],
-
-    //网站Logo
-    siteLogo: "",
-  },
 });
---
+++ src/config.mjs
@@ -0,0 +1,16 @@
+export default{
+
+    title: '🥬 SiteName',
+
+    description: '网站描述',
+
+    logo: '',
+
+    navLinks: {
+        '🏠 首页' : '/',
+        '🥰 友链' : '/link/',
+        '✍️ 留言' : '/ly/',
+        '😎 关于' : '/about/',
+    }
+    
+}
